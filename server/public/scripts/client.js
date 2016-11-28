ee10c564bd98112ace14c5fc1ce9df22378bebc9
Casey Hyde <hyde.casey@gmail.com>
2016-11-28 13:45:24 -0600

    Routes set up and working.

---
+++ server/public/scripts/client.js
@@ -0,0 +1,18 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.config([$routeProvider], function($routeProvider) {
+  $routeProvider
+    .when('/heroentry', {
+      templateUrl: '/views/templates/hero-entry.html',
+      controller: 'HeroEntryController',
+      controllerAs: 'entry'
+    })
+    .when('/heroedit', {
+      templateUrl: '/views/templates/hero-edit.html',
+      controler: 'HeroEditController',
+      controllerAs: 'edit'
+    })
+    .otherwise({
+      redirectTo: 'heroentry'
+    })
+})
